07a7fd501eb6ed20d160646a6660746a37ec7826
Nathan Totten <ntotten@auth0.com>
2017-06-16 14:08:31 -0400

    CDN script

--- tools/cdn.sh
+++ tools/cdn.sh
@@ -1,8 +1,21 @@
-CURRENT_VERSION=$(node -p "require('./package').version")
 
-NPM_NAME=$(node tools/attribute.js name)
-VERSION=$(node tools/attribute.js version)
+CURRENT_VERSION=$(node tools/attribute.js version)
 
-aws s3 ls s3://assets.us.auth0.com/js/analytics/$CURRENT_VERSION/ | grep 'analytics.min.js'
+CDN_EXISTS=$(aws s3 ls s3://assets.us.auth0.com/js/analytics/$CURRENT_VERSION/ | grep 'analytics.min.js')
 
-# aws s3 sync release s3://assets.us.auth0.com/js/analytics/$CURRENT_VERSION --region us-west-1 --quiet --cache-control "max-age=86400" --acl public-read
+verbose()
+{
+  echo -e " \033[36m→\033[0m $1"
+}
+
+success()
+{
+  echo -e " \033[1;32m✔︎\033[0m $1"
+}
+
+if [ ! -z "$CDN_EXISTS" ]; then
+  verbose "There is already a version $CURRENT_VERSION in the cdn. Skipping cdn publish…"
+else
+  aws s3 sync release s3://assets.us.auth0.com/js/analytics/$CURRENT_VERSION --region us-west-1 --quiet --cache-control "max-age=86400" --acl public-read
+  success "$CURRENT_VERSION uploaded to the cdn"
+fi
