ab01377624c4de49f4cd21578cb75a7b2d9454fe
Nathan Totten <ntotten@auth0.com>
2017-06-16 14:03:18 -0400

    path

--- tools/attribute.js
+++ tools/attribute.js
@@ -1,5 +1,5 @@
 var attributeName = process.argv[2] || 'name';
-var path = process.argv[3] || '../../../package.json';
+var path = process.argv[3] || '../package.json';
 var attributes = require(path);
 var value = attributes[attributeName];
 if (value !== undefined) {
