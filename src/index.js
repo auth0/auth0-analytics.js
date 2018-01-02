2bdcbeacc013616d5d7ecc00a9704b3baabd53a2
Franco Correa <franco@francocorrea.com.uy>
2018-01-02 12:13:03 -0300

    Fix #14: Babel Polyfill Error on 1.3.0.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 /* global window, Auth0Lock */
-import 'babel-polyfill';
+
+// Safe require for babel-pollyfill
+// See issue: https://github.com/auth0/auth0-analytics.js/issues/14
+if (
+  (typeof window !== 'undefined' && !window['_babelPolyfill']) ||
+  (typeof global !== 'undefined' && !global['_babelPolyfill'])
+) {
+  require('babel-polyfill');
+}
 
 import TagManager from 'auth0-tag-manager';
 
 let analytics;
 
-const IGNORED_EVENTS = [
-  'hash_parsed'  
-];
+const IGNORED_EVENTS = ['hash_parsed'];
 
 function eventShouldBeIgnored(name) {
-  if (typeof name !== 'string') throw new Error('Lock event name must be a string.');
-  
+  if (typeof name !== 'string')
+    throw new Error('Lock event name must be a string.');
+
   return IGNORED_EVENTS.indexOf(name) !== -1;
 }
 
 function eventIsAvailable(lock, name) {
-  if (typeof name !== 'string') throw new Error('Lock event name must be a string.');
-  
+  if (typeof name !== 'string')
+    throw new Error('Lock event name must be a string.');
+
   return lock.validEvents.indexOf(name) !== -1;
 }
 
 function setupEvent(lock, name, tracker = analytics) {
   if (!eventIsAvailable(lock, name)) return;
-  
+
   lock.on(name, function(payload) {
-    if (name === 'authenticated' && payload && payload.idTokenPayload && payload.idTokenPayload.sub) {
+    if (
+      name === 'authenticated' &&
+      payload &&
+      payload.idTokenPayload &&
+      payload.idTokenPayload.sub
+    ) {
       tracker.setUserId(payload.idTokenPayload.sub);
     }
     let eventName = `auth0 lock ${name}`;
@@ -35,7 +48,9 @@ function setupEvent(lock, name, tracker = analytics) {
 
 function init(lock) {
   if (!window.auth0AnalyticsOptions) {
-    throw new Error('You must provide initialization options for Auth0 Analytics.');
+    throw new Error(
+      'You must provide initialization options for Auth0 Analytics.'
+    );
   }
 
   if (!window.auth0AnalyticsOptions.label) {
@@ -44,7 +59,7 @@ function init(lock) {
 
   analytics = TagManager(window.auth0AnalyticsOptions);
 
-  lock.validEvents.forEach((name) => {
+  lock.validEvents.forEach(name => {
     if (eventShouldBeIgnored(name)) return; // Not needed for analytics
     setupEvent(lock, name);
   });
